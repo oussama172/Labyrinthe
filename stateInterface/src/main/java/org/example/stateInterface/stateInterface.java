624a739f83e255d69483454c886ccb160a32602e
oussama172 <83451468+oussama172@users.noreply.github.com>
2021-06-11 03:07:43 +0100

    Add files via upload

---
+++ stateInterface/src/main/java/org/example/stateInterface/stateInterface.java
@@ -0,0 +1,9 @@
+package org.example.stateInterface;
+
+public interface stateInterface {
+    public void youlooser();
+    public void goUp();
+    public void goDown();
+    public void goLeft();
+    public void goRight();
+}
